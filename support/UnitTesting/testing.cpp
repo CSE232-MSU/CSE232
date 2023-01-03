3c20455e185a7eccd57fe33480cc5862596d2750
Charles Ofria <charles.ofria@gmail.com>
2023-01-03 12:45:38 -0500

    Added more unit testing examples, including one that fails.

--- support/UnitTesting/testing.cpp
+++ support/UnitTesting/testing.cpp
@@ -8,9 +8,15 @@ int main()
 {
   std::cout << "Testing." << std::endl;
 
-  TEST_CASE("Simple Tests", 10.0);
+  TEST_CASE("Simple Tests", 10.0);  // A 10-point test case.
 
   size_t x = 5;
   size_t y = 8;
-  CHECK(x + 2 < y+1);
+  CHECK(x + 2 < y+1, "This is an error message.");
+
+  std::string str = "Test string.";
+  std::string error_middle = " multi-segment";
+  CHECK(str == "Test string.", "This is an error message for a", error_middle, " string test.");
+
+  CHECK(str == "Test string2.", "This is an error message for a string test that should fail.");
 }
