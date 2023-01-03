72f41f763aee44be47020acdfe564c72ee80675e
Charles Ofria <charles.ofria@gmail.com>
2023-01-03 11:05:09 -0500

    Lots of cleanup on unit testing; basic functionality in place.

--- support/UnitTesting/testing.hpp
+++ support/UnitTesting/testing.hpp
@@ -9,12 +9,18 @@
 #ifndef CSE232_UNIT_TESTING
 #define CSE232_UNIT_TESTING
 
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace cse232 {
 
-  // Helper functions.
+  //
+  // ---------------------- Helper functions ----------------------
+  //
+
+  /// Return the first position found for any of a set of substring tests
+  /// (or std::string::npos if none are found).
   template <typename... Ts>
   size_t find_any_of(
     const std::string test_str,
@@ -31,39 +37,92 @@ namespace cse232 {
       return std::min(pos1, pos2);
     }
   }
-  
+
+  //
+  // ---------------------- Main Unit Testing Class ----------------------
+  //
+
   /// Manage a set of unit tests.
   struct UnitTester {
-    size_t check_count = 0;
-
+    // Information about the results of an individual check.  E.g.: CHECK(value == x+5);
     struct CheckInfo {
-      std::string lhs = "";
-      std::string rhs = "";
-      std::string comparator = "";
-      bool passed = false;
-      std::string message = "";  // Message on failure.
+      std::string test = "";       // The original test as it was passed in (e.g., "value == x+5")
+      std::string lhs = "";        // Representation of the left side of a test (e.g., "value")
+      std::string rhs = "";        // Representation of the right side of a test (e.g., "x+5")
+      std::string lhs_value = "";  // Representation of the value on the left (e.g., "20")
+      std::string rhs_value = "";  // Representation of the value on the right (e.g., "21", if x=16)
+      std::string comparator = ""; // Which comparison was used here?  (e.g., "==")
+      bool passed = false;         // Was this check successful?
+      bool resolved = false;       // Are we done performing this check?
+      std::string message = "";    // Extra message on failure (e.g., "Grade assessments do not align.")
     };
 
+    // Information about an entire test case.
     struct CaseInfo {
-      std::string name;
-      double points = 0.0;
-      size_t checks_passed = 0;
-      size_t checks_failed = 0;
-      bool hidden = false;
+      std::string name;            // Unique name for this test case.
+      double points = 0.0;         // Number of points this test case is worth.
+      bool hidden = false;         // Should this test case be seen by students?
 
       std::vector<CheckInfo> checks;
     };
 
+    // The set of all test cases being conducted.
     std::vector<CaseInfo> test_cases;
 
-    // Track the two sides of a comparison (if only one side, rhs will be "")
-    std::string lhs = "";
-    std::string rhs = "";
-    
-    void IncCheckNumber() {
-      check_count++;
+    // Get the current open test case; create a default case if none exist.
+    CaseInfo & GetCase() {
+      // If we do not have a case, added a default one (with a warning.)
+      if (test_cases.size() == 0) {
+        std::cerr << "Added test case `DEFAULT`" << std::endl;
+        test_cases.push_back(CaseInfo{"DEFAULT"});
+      }
+      return test_cases.back();
     }
 
+    // Get the current open check in the current case.
+    CheckInfo & GetCheck() { return GetCase().checks.back(); }
+
+    // A proxy class to store partial (LHS) data from a unit test, while preserving type.
+    template <typename T>
+    struct UnitTestProxy {
+      CheckInfo & check;
+      const T & lhs_v;
+      UnitTestProxy(CheckInfo & _check, const T & _value)
+        : check(_check), lhs_v(_value)
+      {
+        // Store the provided lhs valse in the check.
+        std::stringstream ss;
+        ss << lhs_v;
+        check.lhs_value = ss.str();
+
+        // If the test does not have a RHS, convert LHS to a bool and stop here.
+        if (check.rhs == "") {
+          check.passed = static_cast<bool>(lhs_v);
+          check.resolved = true;
+        }
+      }
+
+      template <typename T2> 
+      bool Resolve(bool result, const T2 & rhs_v) {
+        // Store the right-hand-side value in the check.
+        std::stringstream ss;
+        ss << rhs_v;
+        check.rhs_value = ss.str();
+
+        check.passed = result;
+        check.resolved = true;
+        return check.passed;
+      }
+
+      template <typename T2> bool operator==(const T2 & rhs_v) { return Resolve(lhs_v == rhs_v, rhs_v); }
+      template <typename T2> bool operator!=(const T2 & rhs_v) { return Resolve(lhs_v != rhs_v, rhs_v); }
+      template <typename T2> bool operator< (const T2 & rhs_v) { return Resolve(lhs_v < rhs_v, rhs_v); }
+      template <typename T2> bool operator<=(const T2 & rhs_v) { return Resolve(lhs_v <= rhs_v, rhs_v); }
+      template <typename T2> bool operator> (const T2 & rhs_v) { return Resolve(lhs_v > rhs_v, rhs_v); }
+      template <typename T2> bool operator>=(const T2 & rhs_v) { return Resolve(lhs_v >= rhs_v, rhs_v); }
+    };
+
+    
     CaseInfo & AddTestCase(const std::string & name, double points) {
       test_cases.push_back(CaseInfo{name, points});
       return test_cases.back();
@@ -92,28 +151,42 @@ namespace cse232 {
 
       // Determine which comparison operator we are working with (if any) and the terms being compared.
       CheckInfo check_info;
+      check_info.test = test; // Save the original test.
       if (comp_pos != std::string::npos) {
         check_info.comparator += test[comp_pos];
         if (test[comp_pos+1] == '=') check_info.comparator += "=";
 
         // Identify the left and right hand sides
         check_info.lhs = test.substr(0,comp_pos);
-        check_info.rhs = test.substr(comp_pos+comparator.size());
+        check_info.rhs = test.substr(comp_pos+check_info.comparator.size());
       }
       else {
         check_info.lhs = test;
       }
 
-      // Make sure we have a test case to include this check in.
-      if (test_cases.size() == 0) {
-        std::cerr << "Added test case `DEFAULT`" << std::endl;
-        test_cases.push_back(CaseInfo{"DEFAULT"});
-      }
-
       // Insert this check's information into the current test case.
-      test_cases.back().checks.push_back(check_info);
+      GetCase().checks.push_back(check_info);
     }
 
+    // Tests will be started with the < operator.
+    template <typename T>
+    UnitTestProxy<T> operator<(const T & in) {
+      return UnitTestProxy<T>(GetCheck(), in);
+    }
+
+    // Print the results for the latest check performed.
+    void PrintCheckResults() {
+      CheckInfo & check = GetCheck();
+      std::cout << "ORIGINAL  : " << check.test << std::endl;
+      std::cout << "lhs       : " << check.lhs << std::endl;
+      std::cout << "rhs       : " << check.rhs << std::endl;
+      std::cout << "lhs_value : " << check.lhs_value << std::endl;
+      std::cout << "rhs_value : " << check.rhs_value << std::endl;
+      std::cout << "comparator: " << check.comparator << std::endl;
+      std::cout << "passed    : " << check.passed << std::endl;
+      std::cout << "resolved  : " << check.resolved << std::endl;
+      std::cout << "message   : " << check.message << std::endl;
+    }
   };
 
   /// Create a singleton unit tester.
@@ -123,6 +196,11 @@ namespace cse232 {
   }
   
 }
+
+
+//
+// ---------------------- Unit Testing Macros ----------------------
+//
 
 
 /// Start a new test case with the given number of points.
@@ -142,7 +220,8 @@ namespace cse232 {
 #define CHECK(TEST, ...) {		                          \
     auto & unit_tester = cse232::GetUnitTester();       \
     unit_tester.SetupCheck(#TEST, __LINE__, __FILE__);  \
-    unit_tester << TEST;                                \
+    unit_tester < TEST;                                 \
+    unit_tester.PrintCheckResults();                    \
   }
 
 
